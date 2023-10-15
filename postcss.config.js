eb4ece2974b1905cc5196897b6ea33bfce4fbe92
lcmen <lcmen@users.noreply.github.com>
2023-10-15 18:36:15 +0000

    deploy: 11e9abcee99d3f9dc846ad9e9170818719a49820

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const production = process.env.JEKYLL_ENV == 'production'
-
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(production ? [require('cssnano')({ preset: 'default' })] : [])
-  ]
-}
